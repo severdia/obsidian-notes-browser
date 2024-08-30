9dc7321694436478fa63f86d7af3eb61a60c06c7
Ayyoub-ESSADEQ <ayoube.essadeq@gmail.com>
2024-08-30 03:21:24 +0100

    feat : optimize NotesView Rendering using React virtualized

--- src/components/NotesView/index.tsx
+++ src/components/NotesView/index.tsx
@@ -3,6 +3,7 @@ import { Note } from "components/Note";
 import { IoGridOutline, IoTrashOutline } from "react-icons/io5";
 import { ReactNode } from "react";
 import { FaListUl } from "react-icons/fa";
+import { AutoSizer, List, ListRowProps } from "react-virtualized";
 
 const Tab = ({ children }: { children: ReactNode }) => (
   <div className="onb-h-fit onb-px-2 onb-flex onb-items-center onb-justify-center">
@@ -33,16 +34,31 @@ const NotesViewHeader = (props: React.ComponentProps<"div">) => {
 
 export function NotesView() {
   const files = useStore((state) => state.notes);
+  const notes = files.filter((file) => file.extension == "md");
+  const RowRenderer = (props: ListRowProps) => (
+    <div aria-label="" key={notes[props.index].path} style={props.style}>
+      <Note file={notes[props.index]} />
+    </div>
+  );
 
   return (
     <div className="onb-flex onb-flex-col onb-bg-white onb-h-full onb-w-full  onb-flex-grow">
       <NotesViewHeader />
+
       <div className="onb-w-full onb-h-full onb-p-2 onb-gap-2">
-        {files.length > 0 &&
-          files.map(
-            (file) =>
-              file.extension == "md" && <Note key={file.path} file={file} />
-          )}
+        {files.length > 0 && (
+          <AutoSizer>
+            {({ height, width }) => (
+              <List
+                width={width}
+                height={height}
+                rowCount={notes.length}
+                rowHeight={64}
+                rowRenderer={RowRenderer}
+              />
+            )}
+          </AutoSizer>
+        )}
 
         {files.length === 0 && (
           <div className="onb-w-full onb-h-full onb-flex onb-items-center  onb-justify-center onb-text-gray-400">
